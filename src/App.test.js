9140febb314cee4dd5d86e2a331da888244a41d3
Abriel Cleaver <abrielcleaver@gmail.com>
2022-02-18 13:36:19 -0700

    Update test to check for welcome text on authpage

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders welcome message from AuthPage', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const authPageMessage = screen.getByText(/welcome/i);
+  expect(authPageMessage).toBeInTheDocument();
 });
