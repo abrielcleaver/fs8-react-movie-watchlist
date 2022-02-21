5dd9971d6c1572103f24e78f77b3dc7c85ad2bfb
Abriel Cleaver <abrielcleaver@gmail.com>
2022-02-21 16:12:12 -0700

    Setup SearchPage with form and MovieList import

--- src/Components/SearchPage.js
+++ src/Components/SearchPage.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MovieList from '../Components/Movie/MovieList';
+import { searchMovies } from '../services/fetch-utils';
 
 export default function SearchPage() {
+
+  // call state 
+  const [results, setResults] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
   return (
-    <div>SearchPage</div>
+    <div>SearchPage
+      <form>
+        <input value={searchQuery} onChange={e => setSearchQuery(e.target.value)} />
+        <button>Search</button>
+      </form>
+      <section>
+        Is this what you&apos;re looking for? If not, try searching again!
+        Your results:
+        <MovieList />
+      </section>
+    </div>
   );
 }
