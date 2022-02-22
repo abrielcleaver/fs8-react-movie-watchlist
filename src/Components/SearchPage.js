ff080a3f3bade180c40810d5c0eafc17c7eec95d
Abriel Cleaver <abrielcleaver@gmail.com>
2022-02-22 13:25:26 -0700

    Add handleSearch function

--- src/Components/SearchPage.js
+++ src/Components/SearchPage.js
@@ -1,22 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MovieList from '../Components/Movie/MovieList';
-import { searchMovies } from '../services/fetch-utils';
+import { getWatchList, searchMovies } from '../services/fetch-utils';
 
 export default function SearchPage() {
-
-  // call state 
   const [results, setResults] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
+  const [watchlist, setWatchlist] = useState([]);
+
+  async function handleSearch(e) {
+    e.preventDefault();
+  
+    const movies = await searchMovies(searchQuery);
+
+    setResults(movies);
+  }
+
+  async function refreshWatchList() {
+    const myWatchList = await getWatchList();
+    
+    setWatchlist(myWatchList);
+  }
+
+  useEffect(() => {
+    refreshWatchList();
+  
+  }, []);
+  
+  function isOnWatchList(api_id) {
+    const match = watchlist.find(item => Number(item.api_id) === Number(api_id));
+
+    return Boolean(match);
+  }
+  
+
   return (
     <div>SearchPage
-      <form>
+      <form onSubmit={handleSearch}>
         <input value={searchQuery} onChange={e => setSearchQuery(e.target.value)} />
         <button>Search</button>
       </form>
       <section>
         Is this what you&apos;re looking for? If not, try searching again!
-        Your results:
-        <MovieList />
+       
+        <MovieList movies={results} isOnWatchList={isOnWatchList} refreshWatchList={refreshWatchList} />
       </section>
     </div>
   );
