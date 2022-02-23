93943184ca34800167cc334eab217d2b10aa476d
Abriel Cleaver <abrielcleaver@gmail.com>
2022-02-23 15:24:19 -0700

    Change MovieList component import to fix deploy issue

--- src/Components/Movie/MovieList.js
+++ src/Components/Movie/MovieList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import Movie from './Movie';
-import ListItem from '../List/ListItem';
+import ListItem from '../List/ListItem.js';
 
 // on the search page, show a movie.
 // on the Listpage page, show a watchList item
