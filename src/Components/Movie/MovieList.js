fcaf000d70f9128041ce0a6bb7c547e57dbee35a
Abriel Cleaver <abrielcleaver@gmail.com>
2022-02-22 20:50:12 -0700

    Updata MovieList component function to render movie on the search page and a watchlist item when on the listpage

--- src/Components/Movie/MovieList.js
+++ src/Components/Movie/MovieList.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import Movie from './Movie';
+import ListItem from '../List/ListItem';
 
-export default function MovieList() {
+// on the search page, show a movie.
+// on the listpage page, show a watchlist item
+export default function MovieList({ 
+  movies, 
+  refreshWatchlist, 
+  isOnWatchList,
+}) {
+  const location = useLocation();
+
   return (
-    <div>MovieList</div>
+    <div className='movie-list'>
+      {
+        movies.map((movie, i) => location.pathname.includes('search') 
+          ? <Movie 
+            key={movie.title + i} 
+            movie={movie} 
+            isOnWatchList={isOnWatchList} 
+            refreshWatchList={refreshWatchlist} /> 
+          : <ListItem 
+            key={movie.title + i} 
+            refreshWatchlist={refreshWatchlist}
+            movie={movie} />
+        )}
+    </div>
   );
 }
